8a2c1c3916e515fd6dabcc653d5ed2d2e83966b0
Sam Su <sams@mctoffee.net>
2025-05-18 00:17:24 +0700

    init

---
+++ src/client/java/org/iclab/myphone/client/MyphoneClient.java
@@ -0,0 +1,10 @@
+package org.iclab.myphone.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyphoneClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/iclab/myphone/Myphone.java
@@ -0,0 +1,13 @@
+package org.iclab.myphone;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Myphone implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        System.out.print("");
+    }
+}
